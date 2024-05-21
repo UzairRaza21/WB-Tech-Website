ebcdaba508388b67c9db48f0ffa57ca081281710
Uzair-Raza <uzairraza0@gmail.com>
2024-05-21 23:43:05 +0500

    update

--- Assets/js/wb.js
+++ Assets/js/wb.js
@@ -6,17 +6,40 @@ window.addEventListener('scroll', function() {
     var heroBg = document.querySelector('.header-bg');
     var mainSlider = document.querySelector('.slide-container');
     var scrollPosition = window.scrollY;
-    // Slide-2 
+
+    // Slide-2 Variable
     var ball1 = document.querySelector('.ball-1');
-    var ball2 = document.querySelector('.ball-2')
+    var ball2 = document.querySelector('.ball-2');
+    var rectangle = document.querySelector('.hero-home-slide-2-rectangle');
 
     // Slide- 1 and Slide 2
-    if (scrollPosition > 10 && scrollPosition <= 200){
+    if (scrollPosition > 10 && scrollPosition <= 400){
         slide1.classList.add('scrolled-1');
         slide2.classList.add('scrolled-1-slide-2');
         ball1.classList.add('up');
         ball2.classList.add('down');
-    }
+        rectangle.classList.remove('rotate-up');
+        rectangle.style.opacity = "0";
+        slide3.classList.remove('scrolled-1-slide-3');
+
+        // Slide -2 Animation
+        if (scrollPosition > 100 && scrollPosition < 300){
+            rectangle.classList.add('rotate-up');
+            rectangle.classList.remove('expand');
+            slide3.classList.remove('scrolled-1-slide-3');
+        }
+
+        if(scrollPosition > 301 && scrollPosition < 600){
+            rectangle.classList.remove('rotate-up');
+            rectangle.style.opacity = "1";
+            rectangle.classList.add('expand');
+            slide2.classList.remove('scrolled-1-slide-2');
+            slide3.classList.add('scrolled-1-slide-3');
+        }
+            }
+            // Nested if End
+
+
     else{
         slide1.classList.remove('scrolled-1');
         slide2.classList.remove('scrolled-1-slide-2');
@@ -24,27 +47,18 @@ window.addEventListener('scroll', function() {
         ball2.classList.toggle('down');
     }
 
-    if(scrollPosition > 200){
+
+    if(scrollPosition > 400){
         slide1.style.display = 'none';
     }
     else{
         slide1.style.display = 'block';
     }
 
-    // Slide-2  and Slide 3
-
-    if(scrollPosition > 200){
-        slide3.classList.add('scrolled-1-slide-3');
-    }
-    else{
-        slide3.classList.remove('scrolled-1-slide-3');
-    }
-
-
     // Slide 3 Switch to Normal Website
 
     
-    if(scrollPosition >= 500){
+    if(scrollPosition >= 600){
         mainSlider.classList.add('opacity');
         heroBg.style.position = 'static';
     }
@@ -52,6 +66,7 @@ window.addEventListener('scroll', function() {
         mainSlider.classList.remove('opacity');
         heroBg.style.position = 'fixed';
     }
+
 
 });
 
