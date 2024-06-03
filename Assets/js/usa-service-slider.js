a4e74bdb75e0e2f525be1d20cee5145a94643310
Uzair-Raza <uzairraza0@gmail.com>
2024-06-04 00:50:10 +0500

    udpate

--- Assets/js/usa-service-slider.js
+++ Assets/js/usa-service-slider.js
@@ -1,43 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const slides = document.querySelectorAll('.grow');
+    
+    slides.forEach((slide, index) => {
+        slide.addEventListener('mouseover', () => {
 
-document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll('.usa-service-slide');
+            // Check if it's not the last slide
+            if (index === slides.length - 1) {
+              const prevSlide = slides[index - 1];
+             prevSlide.classList.add('hidden');
+            }       
 
-    slides.forEach(function(slide, index) {
-        slide.addEventListener("mouseenter", function() {
-            // Hide the entire div containing the adjacent image of the expanded slide
-            const expandedSlide = this;
-            const nextSlide = expandedSlide.nextElementSibling;
-            const prevSlide = slides[index - 1];
-            if (nextSlide) {
-                setTimeout(function() {
-                    nextSlide.style.display = 'none';
-                }, 0);
-             
-            }
-            if (index === slides.length - 1 && prevSlide) {
-                setTimeout(function() {
-                    prevSlide.style.display = 'none';
-                }, 0);
+
+            if (index < slides.length - 1) {
+                const nextSlide = slides[index + 1];
+                nextSlide.classList.add('hidden');
+
             }
         });
 
-        slide.addEventListener("mouseleave", function() {
-            // Restore visibility of the adjacent div when mouse leaves the slide
-            const expandedSlide = this;
-            const nextSlide = expandedSlide.nextElementSibling;
-            const prevSlide = slides[index - 1];
-            if (nextSlide) {
-                setTimeout(function() {
-                    nextSlide.style.display = 'block';
-                }, 2500);
-            }
-            if (index === slides.length - 1 && prevSlide) {
-                setTimeout(function() {
-                    prevSlide.style.display = 'block';
-                }, 2500);
+        slide.addEventListener('mouseout', () => {
 
+            // Check if it's the last slide
+        if (index === slides.length - 1) {
+        const prevSlide = slides[index - 1];
+        prevSlide.classList.remove('hidden');
+        }
+            if (index < slides.length - 1) {
+                const nextSlide = slides[index + 1];
+                nextSlide.classList.remove('hidden');
             }
         });
     });
 });
-
